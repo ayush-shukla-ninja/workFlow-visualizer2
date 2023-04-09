b561094466cf36e35df0a9196304a01cdf73206c
NC24305_Ayush <nc24305_ayush@Ninja-ITs-MacBook-Air.local>
2023-04-09 19:56:03 +0530

    bug resolved for ruleConfigExecution in upload

--- src/Components/Landing.tsx
+++ src/Components/Landing.tsx
@@ -33,7 +33,65 @@ const Landing = () => {
       toast.error("Data is not valid");
       return;
     }
-    navigate("/visualize", { state: fileData });
+
+    let fileData1 = JSON.parse(fileData);
+    fileData1 = fileData1;
+    var fetchedData = fileData1;
+    console.log("fetchedData", fetchedData);
+
+    for ( let index1 = 0; index1 < fileData1.apiComponentList.length;
+      index1++
+    ) {
+      const item = fileData1.apiComponentList[index1];
+      console.log(index1);
+      if (item.ruleConfigExecution) {
+        //let index2 = 0;
+        // for (const item1 of item.ruleConfigExecution) {
+        for (
+          let index2 = 0;
+          index2 < item.ruleConfigExecution.length;
+          index2++
+        ) {
+          const item1 = item.ruleConfigExecution[index2];
+          const name = item1.name;
+
+          fetch(`http://localhost:8080/service/rfind/${name}`)
+            .then((response) => response.json())
+            .then((result: any) => {
+              // console.log("lhs", index1, fetchedData.apiComponentList);
+              //console.log("rhs", result);
+              fetchedData.apiComponentList[index1].ruleConfigExecution[index2] =
+                result[0];
+            })
+            .catch((error) => console.log("error", error));
+        }
+      } else if (item.ruleConfigIdentifier) {
+        const name = item.ruleConfigIdentifier.name;
+        fetch(`http://localhost:8080/service/rfind/${name}`)
+          .then((response) => response.json())
+          .then((result: any) => {
+            // console.log("lhs", index1, fetchedData.apiComponentList);
+            //console.log("rhs", result);
+            fetchedData.apiComponentList[index1].ruleConfigIdentifier =
+              result[0];
+          })
+          .catch((error) => console.log("error", error));
+      }
+    }
+    //console.log("data1", fetchedData);
+    setTimeout(function () {
+      navigate("/visualize", { state: JSON.stringify(fetchedData) });
+    }, 1500);
+
+
+
+
+
+
+
+
+
+    //navigate("/visualize", { state: fileData });
     return;
   };
 
